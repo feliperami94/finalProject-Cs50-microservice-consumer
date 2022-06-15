dd336911adab5b5bf7751eb84d930fd2638675fe
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-06-15 18:52:36 -0500

    Configuracion de Rabbit mq y template del usecase de receive

---
+++ src/main/java/com/santiagoposadag/cs50/receiverconsumer/usecases/ReceiveFromUserActionUseCase.java
@@ -0,0 +1,12 @@
+package com.santiagoposadag.cs50.receiverconsumer.usecases;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveFromUserActionUseCase {
+
+    //TODO
+    public void receiveMessage(String message){
+
+    }
+}
